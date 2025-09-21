f269138d4ce64a5b851e10c309acfb44c3cf3f31
Robert Kenney <kenneyrobert109@gmail.com>
2025-09-21 16:39:23 -0400

    worked on parsing the addresses

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import os
 import imaplib
+import re
 from collections import Counter
-
 
 load_dotenv()
 
@@ -17,26 +17,46 @@ mail.login(EMAIL_ACCOUNT, APP_PASSWORD)
 mail.select("INBOX")  
 print("Logged in")
 
-status, data = mail.search(None, "SINCE", "01-Sep-2025")
+status, data = mail.search(None, "BEFORE", "31-Dec-2024")
 ids = data[0].split()
 
 total_messages = len(ids)
-print("pulled %s messages", total_messages)
-senders = Counter()
+print("filtered to %s messages", total_messages)
 
-for i in range(0, 1):
-
+ids_by_addr = {}
+counts_by_addr = Counter()
+batch_size = 1000
+bad_ids = []
+for i in range(0, total_messages, batch_size):
+    max_id = min(i + batch_size - 1, total_messages - 1)
     start = ids[i].decode()
-    end = ids[i + 1].decode()
+    end = ids[max_id].decode()
 
     sequence_set = f"{start}:{end}"
-    
-    status, msg_data = mail.fetch(sequence_set, "(BODY.PEEK[HEADER.FIELDS (FROM)])")
+    print("reading ids ", sequence_set) 
+    status, msgs = mail.fetch(sequence_set, "(BODY.PEEK[HEADER.FIELDS (FROM)])")
     if status != "OK":
         print("Error reading message ", ids[i])
-    print(msg_data)
-    
+    for msg in msgs:
+        if not isinstance(msg, tuple):
+            continue
+        try: 
+            id = re.search(r"(\d+)", msg[0].decode()).group(1)
+            addr_groups = re.search(r"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})", msg[1].decode()) #i ripped this email regex off the web 
+            if len(addr_groups.group()) == 0:
+                bad_ids.append(id)
+                continue
+            addr = addr_groups.group(1)
+            counts_by_addr[addr] += 1 
+            
+            if addr in ids_by_addr:
+                ids_by_addr[addr].append(id)
+            else:
+                ids_by_addr[addr] = [id]
+        except Exception as e:
+            print("error reading the following msg ", msg[1].decode(), e)
 
-print("Top senders:")
-for sender, count in senders.most_common(20):
-    print(count, sender)
+for item, count in counts_by_addr.most_common(total_messages):
+    print(item, count, ids_by_addr[item])
+
+print("BAD IDS: ", bad_ids)
